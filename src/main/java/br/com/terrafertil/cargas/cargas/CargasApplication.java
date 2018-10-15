ed34e5c609237e974b108e1a27774b217dea6ba2
JULIANO ANTONIETTO <juliano.fatec@gmail.com>
2018-10-15 12:10:13 -0300

    primeiro commit

---
+++ src/main/java/br/com/terrafertil/cargas/cargas/CargasApplication.java
@@ -0,0 +1,12 @@
+package br.com.terrafertil.cargas.cargas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargasApplication.class, args);
+	}
+}
